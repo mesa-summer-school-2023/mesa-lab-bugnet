0bf393da5a6bbe58634176b19d4f96aef52fbccc
mesa-summer-school-2023 <141247575+mesa-summer-school-2023@users.noreply.github.com>
2023-08-07 16:13:05 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,35 +1,88 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# http://www.sphinx-doc.org/en/master/config
 
-# -- Project information
+# -- Path setup --------------------------------------------------------------
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./_ext/'))
 
-release = '0.1'
-version = '0.1.0'
 
-# -- General configuration
+# -- Project information -----------------------------------------------------
 
+project = 'MESA'
+copyright = '2022, The MESA Team'
+author = 'The MESA Team'
+release = 'main'
+version = 'main'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'defaults2rst',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
-
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# -- Options for HTML output
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
 
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = 'sphinx_rtd_theme'
 
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -- Additional configuration ------------------------------------------------
+
+# Ensure that autosectionlabel will produce unique names
+autosectionlabel_prefix_document = True
+# Go to depth 3 so options in defaults files get labels
+autosectionlabel_maxdepth = 6
+
+# sphinx_rtd options
+html_theme_options = {
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+    'logo_only': True
+}
+
+# Set master doc
+master_doc = 'index'
+
+# Set logo
+html_logo = 'mesa-logo-200.png'
+
+# Override theme stylesheet
+html_css_files = [
+    'theme_overrides.css',  # overrides for wide tables in RTD theme
+]
+
+
+# set default highlighting language
+highlight_language = 'fortran'
